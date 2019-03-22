3ce46648ab44d6851caa5425cbbe35f943e0c8f1
Brandon Nifong <brandon@tendency.me>
2019-03-22 08:35:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# WP SMTP
+
+[![Latest Stable Version](https://poser.pugx.org/log1x/wp-smtp/v/stable)](https://packagist.org/packages/log1x/wp-smtp) [![Total Downloads](https://poser.pugx.org/log1x/wp-smtp/downloads)](https://packagist.org/packages/log1x/wp-smtp)
+
+WP SMTP is a simple Composer package for handling WordPress SMTP with `.env`. No admin menus or other bloat. Just a simple admin notice to verify your connection when needed and the ability to do a simple task WordPress should probably be handling natively.
+
+## Getting Started
+
+### Requirements
+
+- [Sage](https://github.com/roots/sage) >= 9.0
+- [Bedrock](https://github.com/roots/bedrock)
+- [PHP](https://secure.php.net/manual/en/install.php) >= 7.1.3
+- [Composer](https://getcomposer.org/download/)
+
+### Installation
+
+Install via Composer:
+
+```sh
+composer require log1x/wp-smtp
+```
+
+## Usage
+
+### Configuration
+
+All configuration goes into `.env`.
+
+#### Required
+
+```conf
+WP_SMTP_HOST=mail.example.com  # Host
+WP_SMTP_USERNAME=example       # Username
+WP_SMTP_PASSWORD=secure123     # Password
+```
+
+#### Optional
+
+```conf
+WP_SMTP_PORT=587                      # Port
+WP_SMTP_PROTOCOL=tls                  # Protocol
+WP_SMTP_TIMEOUT=10                    # Timeout
+WP_SMTP_FORCEFROM=example@example.com # Force From Email
+WP_SMTP_FORCEFROMNAME=Example         # Force From Name
+```
+
+#### Mailhog
+
+```conf
+WP_SMTP_HOST=localhost
+WP_SMTP_PORT=1025
+WP_SMTP_PROTOCOL=false
+```
+
+## Debugging
+
+> SMTP connect() failed.
+
+This error means the initial connection to your host/port failed.
+
+## Contributing
+
+Contributing whether it be through PRs, reporting an issue, or suggesting an idea is encouraged and appreciated. When contributing code, please follow the existing code style.
+
+If you're feeling generous, I also take contributions in the form of [coffee & energy drinks](https://www.buymeacoffee.com/log1x).
+
+## License
+
+Sage SMTP is provided under the [MIT License](https://github.com/log1x/wp-smtp/blob/master/LICENSE.md).
---
+++ src/SMTP.php
@@ -0,0 +1,229 @@
+<?php
+
+namespace Log1x\SMTP;
+
+if (class_exists('SMTP')) {
+    return;
+}
+
+/**
+ * WordPress SMTP
+ */
+class SMTP
+{
+    /**
+     * Default Config
+     *
+     * @var array
+     */
+    protected $config = [
+        'auth' => true,
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed'	=> true
+        ],
+        'timeout' => 10,
+        'host' => '',
+        'port' => 587,
+        'protocol' => 'tls',
+        'username' => '',
+        'password' => '',
+        'forceFrom' => '',
+        'forceFromName' => '',
+    ];
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->admin();
+        $this->mail();
+    }
+
+    /**
+     * Config
+     *
+     * @return object
+     */
+    protected function config()
+    {
+        $config = collect([
+            'host' => env('WP_SMTP_HOST'),
+            'port' => env('WP_SMTP_PORT'),
+            'protocol' => env('WP_SMTP_PROTOCOL'),
+            'username' => env('WP_SMTP_USERNAME'),
+            'password' => env('WP_SMTP_PASSWORD'),
+            'timeout' => env('WP_SMTP_TIMEOUT'),
+            'forceFrom' => env('WP_SMTP_FORCEFROM'),
+            'forceFromName' => env('WP_SMTP_FORCEFROMNAME')
+        ])->filter();
+
+        return (object) collect($this->config)
+            ->merge($config)
+            ->all();
+    }
+
+    /**
+     * Validate Hash
+     *
+     * @return boolean
+     */
+    protected function validate()
+    {
+        return $this->hash() === get_option('wp_mail_verify');
+    }
+
+    /**
+     * Hash
+     *
+     * @return string
+     */
+    protected function hash()
+    {
+        return hash('crc32', serialize($this->config()));
+    }
+
+    /**
+     * Notice
+     *
+     * @param  string $message
+     * @param  string $type
+     * @param  boolean $dismissible
+     * @return void
+     */
+    protected function notice($message, $type = 'info', $dismissible = false)
+    {
+        add_action('admin_notices', function () use ($message, $type, $dismissible) {
+            printf(
+                '<div class="%1$s"><p>%2$s</p></div>',
+                esc_attr("notice notice-{$type}" . ($dismissible ? ' is-dismissible' : '')),
+                __($message, 'wp-smtp')
+            );
+        });
+    }
+
+    /**
+     * Admin
+     *
+     * @return void
+     */
+    protected function admin()
+    {
+        if (! empty($_GET['verify-smtp']) && $_GET['verify-smtp'] === 'true' && wp_verify_nonce($_GET['_wpnonce'], 'verify-smtp')) {
+            $this->verify();
+        }
+
+        if (! $this->config()->username || ! $this->config()->password || ! $this->config()->host) {
+            return $this->notice(
+                'WP SMTP failed to find your SMTP credentials. Please define them in <code>.env</code> and <a href="'.wp_nonce_url(admin_url(add_query_arg('verify-smtp', 'true', 'index.php')), 'verify-smtp').'">click here</a> to test your configuration.',
+                'error'
+            );
+        }
+
+        if (! get_option('wp_mail_verify')) {
+            return $this->notice(
+                'WP SMTP credentials found. Please <a href="'.wp_nonce_url(admin_url(add_query_arg('verify-smtp', 'true', 'index.php')), 'verify-smtp').'">click here</a> to test your configuration.'
+            );
+        }
+
+        if (! $this->validate()) {
+            return $this->notice(
+                'WP SMTP has detected a change in your credentials. Please <a href="'.wp_nonce_url(admin_url(add_query_arg('verify-smtp', 'true', 'index.php')), 'verify-smtp').'">click here</a> to test your configuration.'
+            );
+        }
+    }
+
+    /**
+     * Mail
+     *
+     * @return void
+     */
+    protected function mail()
+    {
+        if ($this->config()->username && $this->config()->password && $this->config()->host) {
+            add_action('phpmailer_init', function ($mail) {
+                $mail->isSMTP();
+                $mail->SMTPAuth = $this->config()->auth;
+                $mail->SMTPSecure = $this->config()->secure;
+                $mail->SMTPOptions = ['ssl' => $this->config()->ssl];
+                $mail->Timeout = $this->config()->timeout;
+
+                $mail->Host = $this->config()->host;
+                $mail->Port = $this->config()->port;
+                $mail->Username = $this->config()->username;
+                $mail->Password = $this->config()->password;
+
+                $mail->setFrom($mail->From, $mail->FromName);
+
+                if ($this->config()->forceFrom && $this->config()->forceFromName) {
+                    $mail->setFrom($this->config()->forceFrom, $this->config()->forceFromName);
+                }
+            }, PHP_INT_MAX);
+        }
+    }
+
+    /**
+     * Verify SMTP Credentials
+     *
+     * @return void
+     */
+    protected function verify()
+    {
+        require_once(ABSPATH . WPINC . '/class-phpmailer.php');
+        $mail = new \PHPMailer(true);
+
+        try {
+            $mail->isSMTP();
+            $mail->SMTPAuth = $this->config()->auth;
+            $mail->SMTPOptions = ['ssl' => $this->config()->ssl];
+            $mail->SMTPSecure = $this->config()->protocol;
+            $mail->SMTPAutoTLS = false;
+
+            $mail->Host = $this->config()->host;
+            $mail->Username = $this->config()->username;
+            $mail->Password = $this->config()->password;
+            $mail->Port = $this->config()->port;
+            $mail->Timeout = $this->config()->timeout;
+
+            $mail->setFrom('no-reply@'.preg_replace('/https?:\/\/(www\.)?/', '', get_bloginfo('url')), get_bloginfo('name'));
+            $mail->AddAddress(wp_get_current_user()->user_email);
+
+            if ($this->config()->forceFrom && $this->config()->forceFromName) {
+                $mail->setFrom($this->config()->forceFrom, $this->config()->forceFromName);
+            }
+
+            $mail->CharSet = get_bloginfo('charset');
+            $mail->Subject = 'WP SMTP Validation';
+            $mail->Body	= 'Success.';
+
+            $mail->Send();
+            $mail->ClearAddresses();
+            $mail->ClearAllRecipients();
+        } catch (\phpMailerException $error) {
+            return $this->notice(
+                $error->errorMessage(),
+                'error', true
+            );
+        } catch (Exception $error) {
+            return $this->notice(
+                $error->getMessage(),
+                'error',
+                true
+            );
+        }
+
+        if (update_option('wp_mail_verify', $this->hash())) {
+            return $this->notice(
+                'WP SMTP connection successful!',
+                'success',
+                true
+            );
+        }
+    }
+}
+
+if (function_exists('add_action')) {
+    return new SMTP;
+}
